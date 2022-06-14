17e43f1289f3f9da4c5e76ad8e1e500c6bdb6e75
lufter21 <lufter21@gmail.com>
2022-06-14 16:06:31 +0300

    cats

--- src/components/Main/MainCases/MainCases.jsx
+++ src/components/Main/MainCases/MainCases.jsx
@@ -3,29 +3,46 @@ import { Link } from 'react-router-dom';
 import "./MainCases.scss";
 import db from "../../../DataBase/Live.json";
 
-function MainCases(props) {
+const Case = function (props) {
+  return (
+    <Link to={`casePage/${props.id}`} className="case-item" key={props.id}>
+      <div className="case">
+        <div className="case-card">
+          <div className="case-card-hover"></div>
+          <div className="case-card__img">
+            <img src={props.image} alt="case-card" />
+          </div>
+          <div className="case-card__info">
+            <span className="case-card_name">{props.name}</span>
+            <span className="case-card_price">{props.price}р</span>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+const Category = function (props) {
+  const items = props.items.map(item => <Case {...item} key={item.id} />);
+
+  return (
+    <div className="cases-category">
+      <div className="cases-category__title">
+        {props.catTitle}
+      </div>
+      <div className="cases">
+        {items}
+      </div>
+    </div>
+  );
+}
+
+function MainCases() {
+  const categories = db["cases"].map(cat => <Category {...cat} key={cat.catId} />);
+
   return (
     <div className="main-wrapper">
-      <div className="cases">
-        {db["cases"].map((caseDb) => {
-          return (
-            <Link to={`casePage/${caseDb.id}`} className="case-item" key={caseDb.id}>
-              <div className="case">
-                <div className="case-card">
-                  <div className="case-card-hover"></div>
-                  <div className="case-card__img">
-                    <img src={caseDb.image} alt="case-card"/>
-                  </div>
-                  <div className="case-card__info">
-                    <span className="case-card_name">{caseDb.name}</span>
-                    <span className="case-card_price">{caseDb.price}р</span>
-                  </div>
-                </div>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+      {categories}
     </div>
   );
 }
