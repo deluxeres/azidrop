4621a36671df994caf12c50a4e1d313175b5b471
Vitalii Bokhin <vitaliibokhin@gmail.com>
2022-08-07 01:50:42 +0300

    fix

--- src/functions/getBadge.js
+++ src/functions/getBadge.js
@@ -3,78 +3,7 @@ const getBadge = function (name, tit) {
         return 'orange';
     }
 
-    switch (name && name.toLowerCase()) {
-        case 'базового класса':
-            return 'silver';
-        case 'засекреченное':
-            return 'purple';
-        case 'армейское качество':
-            return 'blue';
-        case 'запрещенное':
-            return 'maline';
-        case 'ширпотреб':
-            return 'silver';
-        case 'промышленное качество':
-            return 'bluelight';
-        case 'тайное':
-            return 'red';
-        case 'высшего класса':
-            return 'blue';
-        case 'примечательного типа':
-            return 'purple';
-        case 'экзотичного вида':
-            return 'maline';
-        case 'consumer grade':
-            return 'silver';
-        case 'контрабандное':
-            return 'orange';
-        case 'контрабанда':
-            return 'orange';
-        case 'экстраординарного типа':
-            return 'red';
-        case 'restricted':
-            return 'maline';
-        case 'mil-spec grade':
-            return 'blue';
-        case 'classified':
-            return 'red';
-        case 'industrial grade':
-            return 'bluelight';
-        case 'covert':
-            return 'red';
-        case 'high grade':
-            return 'blue';
-        case 'remarkable':
-            return 'red';
-        case 'extraordinary':
-            return 'red';
-        case 'exotic':
-            return 'red';
-        case 'base grade':
-            return 'blue';
-        case 'distinguished':
-            return 'maline';
-        case 'исключительный':
-            return 'maline';
-        case 'superior':
-            return 'purple';
-        case 'master':
-            return 'red';
-        case 'мастерский':
-            return 'red';
-        case 'заслуженный':
-            return 'blue';
-        case 'blue':
-            return 'orange';
-        case 'exceptional':
-            return 'maline';
-        case 'Нeoбычaйнo peдкиe':
-            return 'orange';
-        case '★':
-            return 'orange';
-        default:
-            return 'silver';
-    }
+    return name ? name : 'silver';
 }
 
 export const getBadgeClass = (name, tit) => 'case-badge__' + getBadge(name, tit);
